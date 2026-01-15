274d31de4b876a7c86d7ca9028ca06374c2a339e
Akshit Nalwaya <nalwayaakshit2403@gmail.com>
2026-01-15 15:35:19 +0530

    Created the question now linking them soon

---
+++ quiz_app/lib/answer_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+
+  const AnswerButton(
+    this.answerText,
+    this.onTap, {
+    super.key,
+  });
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(
+    BuildContext context,
+  ) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.amber,
+        foregroundColor: Colors.deepPurple,
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        // textStyle: Colors.amberAccent,
+      ),
+      child: Text(
+        answerText,
+      ),
+    );
+  }
+}
--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -1,17 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
-class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+class QuestionsScreen
+    extends
+        StatefulWidget {
+  const QuestionsScreen({
+    super.key,
+  });
 
   @override
-  State<QuestionsScreen> createState() {
+  State<
+    QuestionsScreen
+  >
+  createState() {
     return _QuestionsScreenState();
   }
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
-  Widget build(BuildContext context) {
-    return const Text("Inside the questions");
+  Widget build(
+
+  
+    context,
+  ) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Heloo",
+            style: TextStyle(
+              color: Colors.amberAccent,
+              fontSize: 45,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          AnswerButton('Looking for the answer', (){})
+        ],
+      ),
+    );
   }
 }
