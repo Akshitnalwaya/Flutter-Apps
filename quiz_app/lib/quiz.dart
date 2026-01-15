29cb5c8cd68209b9600e5a230ca00bc80d6aeb44
Akshit Nalwaya <nalwayaakshit2403@gmail.com>
2026-01-15 14:31:19 +0530

    Ternary-Operation on quiz.dart

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -2,45 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
-class Quiz extends StatefulWidget {
-  Quiz({super.key});
+class Quiz
+    extends
+        StatefulWidget {
+  const Quiz({
+    super.key,
+  });
 
   @override
-  State<Quiz> createState() {
+  State<
+    Quiz
+  >
+  createState() {
     return _QuizState();
   }
 }
 
-class _QuizState extends State<Quiz> {
-  Widget ? activeScreen;
+class _QuizState
+    extends
+        State<
+          Quiz
+        > {
+  var activeScreen = 'start-screen';
 
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  // @override
+  // void initState() {
+  //   activeScreen = StartScreen(switchScreen);
+  //   super.initState();
+  // }
 
   void switchScreen() {
-    setState(() {
-      activeScreen = QuestionsScreen();
-    });
+    setState(
+      () {
+        activeScreen = 'question-screen';
+      },
+    );
   }
 
   @override
-  Widget build(context) {
+  Widget build(
+    context,
+  ) {
     return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
-                Color.fromARGB(255, 45, 15, 96),
-                Color.fromARGB(255, 128, 82, 208),
+                Color.fromARGB(
+                  255,
+                  45,
+                  15,
+                  96,
+                ),
+                Color.fromARGB(
+                  255,
+                  128,
+                  82,
+                  208,
+                ),
               ],
               begin: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          child:
+              activeScreen ==
+                  'start-screen'
+              ? StartScreen(
+                  switchScreen,
+                )
+              : const QuestionsScreen(),
         ),
       ),
     );
