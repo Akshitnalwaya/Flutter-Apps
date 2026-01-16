b2dac9fec465270fee308172229e8adde0b3f7a8
Akshit Nalwaya <nalwayaakshit2403@gmail.com>
2026-01-16 18:33:59 +0530

    Result Screen Setting uo

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/result_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -13,21 +14,14 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  var activeScreen = 'start-screen';
-
-  // @override
-  // void initState() {
-  //   activeScreen = StartScreen(switchScreen);
-  //   super.initState();
-  // }
-
+  String activeScreen = 'start-screen';
   List<String> selectedAnswers = [];
 
-  void chooseAnser(String answer) {
+  void chooseAnswer(String answer) {
     selectedAnswers.add(answer);
+
     if (selectedAnswers.length == questions.length) {
       setState(() {
-        selectedAnswers = [];
         activeScreen = 'result-screen';
       });
     }
@@ -39,24 +33,44 @@ class _QuizState extends State<Quiz> {
     });
   }
 
-
-
+  void restartQuiz() {
+    setState(() {
+      selectedAnswers = [];
+      activeScreen = 'start-screen';
+    });
+  }
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
+    Widget screenWidget;
+
+    if (activeScreen == 'start-screen') {
+      screenWidget = StartScreen(switchScreen);
+    } else if (activeScreen == 'question-screen') {
+      screenWidget = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    } else {
+      screenWidget = ResultScreen(
+        selectedAnswers: selectedAnswers,
+        onRestart: restartQuiz,
+      );
+    }
 
     return MaterialApp(
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
-              colors: [Color.fromARGB(255, 45, 15, 96), Color.fromARGB(255, 128, 82, 208)],
+              colors: [
+                Color.fromARGB(255, 45, 15, 96),
+                Color.fromARGB(255, 128, 82, 208),
+              ],
               begin: Alignment.bottomRight,
+              end: Alignment.topLeft,
             ),
           ),
-          child: activeScreen == 'start-screen'
-              ? StartScreen(switchScreen)
-              : QuestionsScreen(onSelectAnswer: chooseAnser),
+          child: screenWidget,
         ),
       ),
     );
--- quiz_app/lib/result_screen.dart
+++ quiz_app/lib/result_screen.dart
@@ -4,7 +4,11 @@ class ResultScreen extends StatelessWidget {
   final List<String> selectedAnswers;
   final VoidCallback onRestart;
 
-  const ResultScreen({super.key, required this.selectedAnswers, required this.onRestart});
+  const ResultScreen({
+    super.key,
+    required this.selectedAnswers,
+    required this.onRestart,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -15,17 +19,48 @@ class ResultScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("You are at the end!"),
-            SizedBox(height: 30),
+            const Text("You are at the end!"),
+            const SizedBox(height: 30),
             Text("You answered ${selectedAnswers.length} questions."),
-            SizedBox(height: 30),
-            TextButton(onPressed: onRestart, child: const Text("Restart Quiz")),
+            const SizedBox(height: 30),
+            TextButton(
+              onPressed: onRestart,
+              child: const Text("Restart Quiz"),
+            ),
           ],
         ),
       ),
     );
   }
 }
+
+
+// class ResultScreen extends StatelessWidget {
+//   final List<String> selectedAnswers;
+//   final VoidCallback onRestart;
+
+//   const ResultScreen({super.key, required this.selectedAnswers, required this.onRestart});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return SizedBox(
+//       width: double.infinity,
+//       child: Container(
+//         margin: const EdgeInsets.all(20),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text("You are at the end!"),
+//             SizedBox(height: 30),
+//             Text("You answered ${selectedAnswers.length} questions."),
+//             SizedBox(height: 30),
+//             TextButton(onPressed: onRestart, child: const Text("Restart Quiz")),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 
 // import 'package:flutter/material.dart';
