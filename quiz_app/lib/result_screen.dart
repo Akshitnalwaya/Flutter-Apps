703753dd73bc564fea742bfa0c35aede7d7e06a0
Akshit Nalwaya <nalwayaakshit2403@gmail.com>
2026-01-19 13:09:09 +0530

    Final test

--- quiz_app/lib/result_screen.dart
+++ quiz_app/lib/result_screen.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
+import 'package:quiz_app/question_summary.dart';
 
 class ResultScreen extends StatelessWidget {
   final List<String> selectedAnswers;
   final VoidCallback onRestart;
 
-  const ResultScreen({
-    super.key,
-    required this.selectedAnswers,
-    required this.onRestart,
-  });
+  const ResultScreen({super.key, required this.selectedAnswers, required this.onRestart});
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i < selectedAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': selectedAnswers[i],
+      });
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
+    final summaryData = getSummaryData();
+    final numberTotalQuestions = questions.length;
+    final numCorrectQuestions = summaryData.where((data) {
+      return data['user_answer'] == data['correct_answer'];
+    }).length;
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -19,14 +35,11 @@ class ResultScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text("You are at the end!"),
+            Text("You are at the end and you answered $numCorrectQuestions out of $numberTotalQuestions questions"),
             const SizedBox(height: 30),
-            Text("You answered ${selectedAnswers.length} questions."),
+            QuestionSummary(summaryData),
             const SizedBox(height: 30),
-            TextButton(
-              onPressed: onRestart,
-              child: const Text("Restart Quiz"),
-            ),
+            TextButton(onPressed: onRestart, child: const Text("Restart Quiz")),
           ],
         ),
       ),
