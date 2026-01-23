ff46c8f3c5d0845de7623ce8976ddbead231bd4b
Akshit Nalwaya <nalwayaakshit2403@gmail.com>
2026-01-23 16:41:23 +0530

    Theme added

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -12,13 +12,20 @@ void main() {
         // scaffoldBackgroundColor: Colors.amberAccent,
         colorScheme: kColorsScheme,
         appBarTheme: AppBarTheme().copyWith(
-          backgroundColor: kColorsScheme.onPrimaryContainer,
+          backgroundColor: Colors.amberAccent,
+          // backgroundColor: kColorsScheme.secondaryContainer,
+          // backgroundColor: kColorsScheme.primaryContainer,
+          //   backgroundColor: kColorsScheme.onPrimaryContainer,
+          //   backgroundColor: kColorsScheme.onPrimary,
+          elevation: 40,
           foregroundColor: kColorsScheme.primaryContainer,
+        //   shadowColor: Colors.black,
         ),
-        // cardTheme: CardThemeData(
-        //   color: kColorsScheme.onPrimaryContainer,
-        //   margin: EdgeInsets.all(16),
-        // )
+        cardTheme: CardThemeData(
+          color: kColorsScheme.secondary,
+          margin: EdgeInsets.all(16),
+          shadowColor: Colors.black12
+        )
       ),
       home: Expenses(),
     ),
